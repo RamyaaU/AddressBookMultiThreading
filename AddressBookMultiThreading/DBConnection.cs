6c72375d8f715fca965b5ea21cbf36f7b2b9a6e6
Ramya.U <ramyayadav2016@gmail.com>
2020-12-01 14:44:53 +0530

    [Ramya] Add . Added method to update details

---
+++ AddressBookMultiThreading/DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookMultiThreading
+{
+    public class DBConnection
+    {
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection GetConnection()
+        {
+            /// Specifying the connectionString from the sql server connection.
+            string connectiongString = @"Data Source=RAMYA\SQLEXPRESS;Initial Catalog=addressbook_service;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectiongString);
+            return connection;
+        }
+    }
+}
--- AddressBookMultiThreading/Program.cs
+++ AddressBookMultiThreading/Program.cs
@@ -11,6 +11,13 @@ namespace AddressBookMultiThreading
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Address Book Multi Threading!");
+            ///Creating instance class of AddressBookRepository class.
+            AddressBookRepository repository = new AddressBookRepository();
+            ///Retrieve all the contact details from the DB
+            repository.RetrieveAllContactDetails();
+            ///Updates the column specified of the existing contact using name.
+            Console.WriteLine(repository.UpdateExistingContactUsingByName("Akhilesh", "Roy", "Akhilesh@gmail.com") ? "Updated Succesfully" : "Updated Failed");
         }
     }
 }
+      

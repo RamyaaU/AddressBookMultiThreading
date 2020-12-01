a6bff839c58a8050e5950ea4ab0ea89468b8dfdb
Ramya.U <ramyayadav2016@gmail.com>
2020-12-01 14:38:31 +0530

    [Ramya] Add . Added method to retrieve data

--- AddressBookMultiThreading/Program.cs
+++ AddressBookMultiThreading/Program.cs
@@ -11,6 +11,10 @@ namespace AddressBookMultiThreading
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Address Book Multi Threading!");
+            ///Creating instance class of AddressBookRepository class.
+            AddressBookRepository repository = new AddressBookRepository();
+            ///Retrieve all the contact details from the DB
+            repository.RetrieveAllContactDetails();
         }
     }
 }
